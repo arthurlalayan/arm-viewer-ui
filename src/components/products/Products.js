87863d6a5e62c00e49fe3423d0839c2b94f85a05
Arthur Lalayan <arthurlalayan97@gmail.com>
2021-12-14 11:59:49 +0400

    added some product examples

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -6,6 +6,13 @@ import RadioGroup from '@mui/material/RadioGroup';
 import { useDispatch } from "react-redux";
 import { changeProduct } from "../../features/sidebar/sidebarSlice";
 
+const products = [
+    { id: 1, name: "Product 1", layer: 'arm_1' },
+    { id: 2, name: "Product 2", layer: 'arm_2' },
+    { id: 3, name: "Product 3", layer: 'arm_3' },
+    { id: 4, name: "Product 4", layer: 'arm_4' },
+];
+
 export default function Products() {
     const dispatch = useDispatch();
     return (
@@ -13,8 +20,9 @@ export default function Products() {
             <FormLabel component="legend">Products</FormLabel>
             <RadioGroup aria-label="product" name="radio-buttons-group" defaultValue="none">
                 <FormControlLabel value="none" control={<Radio onClick={() => dispatch(changeProduct({ product: undefined }))} />} label="None" />
-                <FormControlLabel value="product_1" control={<Radio onClick={() => dispatch(changeProduct({ product: { id: 1, name: 'Product 1', layer: 'arm' } }))} />} label="Product 1" />
-                {/* <FormControlLabel value="product_2" control={<Radio onClick={() => dispatch(changeProduct({ product: { id: 2, name: 'Product 2' } }))} />} label="Product 2" /> */}
+                {products.map(product => (
+                    <FormControlLabel key={product.id} value={product.id} control={<Radio onClick={() => dispatch(changeProduct({ product }))} />} label={product.name} />
+                ))}
             </RadioGroup>
         </FormControl>
     );
